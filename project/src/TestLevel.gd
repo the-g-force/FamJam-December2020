0b5ca1308e8c0ce3d8a7da0d5d7a1c783a553ad1
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-12-21 12:49:47 -0500

    Fix whitespace

--- project/src/TestLevel.gd
+++ project/src/TestLevel.gd
@@ -8,13 +8,15 @@ onready var _tiles := $Tiles
 func _ready():
 	for x in range(0,1100,100):
 		_create_tile_at(x)
-		
+
+
 # warning-ignore:return_value_discarded
 func _create_tile_at(x:float)->void:
 	var new_tile := TILE.instance()
 	new_tile.position.x = x
 	_tiles.add_child(new_tile)
 	new_tile.connect("tile_entered", self, "_on_Tile_entered", [new_tile], CONNECT_ONESHOT)
-		
+
+
 func _on_Tile_entered(tile:Node2D):
 	call_deferred("_create_tile_at", tile.position.x + 900)
