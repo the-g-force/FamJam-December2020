af91e94ffc89897b96cecf54c203beb33af90061
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-12-21 09:46:46 -0500

    Implement variable jump length

--- project/src/Elf.gd
+++ project/src/Elf.gd
@@ -1,15 +1,22 @@
 extends KinematicBody2D
 
-export var GRAVITY := 350
+export var GRAVITY := 900
+export var jump_power := 500
+
+# The amount by which the upward velocity is scaled if the jump
+# button is released.
+export var short_jump_velocity_scale := 0.4
 
 var _velocity := Vector2.ZERO
 
 func _physics_process(delta:float):
 	_velocity.y += GRAVITY * delta
 	
-	var y_direction := -1 if is_on_floor() and Input.is_action_just_pressed("jump") else 0
-	
-	_velocity.y += y_direction * 400
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		_velocity.y = -jump_power
+		
+	if _velocity.y < 0.0 and Input.is_action_just_released("jump"):
+		_velocity.y *= short_jump_velocity_scale
 	
 	move_and_slide(_velocity, Vector2.UP)
 
