a2d9773f04db2253ccc58b1690ad005c04200d68
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-12-21 14:34:21 -0500

    Add random ornament animations

--- project/src/Ornament.gd
+++ project/src/Ornament.gd
@@ -2,8 +2,15 @@ extends Area2D
 
 onready var sprite := $Sprite
 
+
 func _ready():
-	var image = "FOO"
+	var animations := []
+	for animation in sprite.frames.get_animation_names():
+		animations.append(animation)
+	var anim_index := randi()%animations.size()
+	var anim:String = animations[anim_index]
+	sprite.play(anim)
+
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
