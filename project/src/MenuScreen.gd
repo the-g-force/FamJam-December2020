3d16d1b69219e8b30f18f7eddf83c8e8ff81aa26
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-12-21 15:55:32 -0500

    Add fullscreen toggle

--- project/src/MenuScreen.gd
+++ project/src/MenuScreen.gd
@@ -1,8 +1,16 @@
 extends Control
 
+onready var _full_screen_toggle := $mainscreen/FullScreenToggle
+
 func _ready():
 	Jukebox.play_just_bells()
+	_full_screen_toggle.pressed = OS.window_fullscreen
+
 
 # warning-ignore:return_value_discarded
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://src/TestLevel.tscn")
+
+
+func _on_FullScreenToggle_pressed():
+	OS.window_fullscreen = not OS.window_fullscreen
