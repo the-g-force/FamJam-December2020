a6ec3e177710a5262f6a0d2041e7841f392b43e5
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-12-21 11:15:10 -0500

    Generate continuous flat surface tiles

--- project/src/TestLevel.gd
+++ project/src/TestLevel.gd
@@ -6,27 +6,27 @@ const TILE : PackedScene = preload("res://src/Tile.tscn")
 export var tile_spawn_delay := 1.0
 export var tile_speed := 100.0
 
+
 var was_gap_last_cycle := false
 
-onready var _timer := $Timer
+var _progress : float = 0
+var _until_next_tile := 0
+
 onready var _tiles := $Tiles
 
 func _ready():
 	randomize()
-	_timer.start(tile_spawn_delay)
 
 
 func _process(delta):
+	_progress += tile_speed * delta
+	_until_next_tile -= tile_speed * delta
+	if _until_next_tile <= 0:
+		var new_tile = TILE.instance()
+		new_tile.position.x = 1000 + _until_next_tile
+		_until_next_tile += new_tile.width()
+		_tiles.add_child(new_tile)
+	
 	for tile in _tiles.get_children():
 		tile.position.x -= tile_speed*delta
 
-
-func _on_Timer_timeout():
-	var spawn_tile := randi()%3
-	if spawn_tile != 0 or (spawn_tile == 0 and was_gap_last_cycle):
-		var Tile:Node2D = TILE.instance()
-		Tile.position = Vector2(1024, 400)
-		_tiles.add_child(Tile)
-		was_gap_last_cycle = false
-	elif spawn_tile == 0 and not was_gap_last_cycle:
-		was_gap_last_cycle = true
--- project/src/Tile.gd
+++ project/src/Tile.gd
@@ -4,6 +4,6 @@ func _process(_delta):
 	if position.x + $CollisionShape2D.shape.extents.x*2 < 0:
 		queue_free()
 
-func _draw():
-	var extents:Vector2 = $CollisionShape2D.shape.extents
-	draw_rect(Rect2(-extents, extents*2), Color.brown)
+
+func width()->float:
+	return 100.0
