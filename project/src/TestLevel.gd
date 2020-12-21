2a00066153f32b5077be7b5c3cc26131e4e258a7
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-12-21 13:08:21 -0500

    Fix tile generation
    This uses a system of triggers that, when hit, generate the next tile in sequence

--- project/src/TestLevel.gd
+++ project/src/TestLevel.gd
@@ -1,22 +1,45 @@
 extends Node2D
 
+const INITIAL_TILES := 8
+const Tile : PackedScene = preload("res://src/Tile.tscn")
+const TileGenerationTrigger : PackedScene = preload("res://src/TileGenerationTrigger.tscn")
 
-const TILE : PackedScene = preload("res://src/Tile.tscn")
+var _tile_index := 0
 
 onready var _tiles := $Tiles
 
 func _ready():
 	for x in range(0,1100,100):
+		_run_tile_generator(x)
+
+
+func _run_tile_generator(x:float)->void:
+	# The initial run of tiles is always there
+	if _tile_index <= INITIAL_TILES:
 		_create_tile_at(x)
+		
+	# Then potentially make a tile, based on the generation algorithm
+	elif (_tile_index - INITIAL_TILES)%3>0:
+		_create_tile_at(x)
+		
+	# Whether we made a tile or not, drop a trigger than will make a new tile when necessary
+	var trigger := TileGenerationTrigger.instance()
+	trigger.position.x = x
+	var _ignored := trigger.connect("body_entered", self, "_on_TileGenerationTrigger_entered", [trigger], CONNECT_ONESHOT)
+	_tiles.add_child(trigger)
+	
+	_tile_index += 1
 
 
 # warning-ignore:return_value_discarded
 func _create_tile_at(x:float)->void:
-	var new_tile := TILE.instance()
+	var new_tile := Tile.instance()
 	new_tile.position.x = x
-	_tiles.add_child(new_tile)
-	new_tile.connect("tile_entered", self, "_on_Tile_entered", [new_tile], CONNECT_ONESHOT)
+	_tiles.add_child(new_tile)	
 
 
-func _on_Tile_entered(tile:Node2D):
-	call_deferred("_create_tile_at", tile.position.x + 900)
+func _on_TileGenerationTrigger_entered(_body, trigger:Node2D):
+	call_deferred("_run_tile_generator", trigger.position.x + 900)
+	trigger.queue_free()
+
+
--- project/src/Tile.gd
+++ project/src/Tile.gd
@@ -1,15 +1,5 @@
 extends StaticBody2D
 
-signal tile_entered
-
-func _process(_delta):
-	if position.x + $CollisionShape2D.shape.extents.x*2 < 0:
-		queue_free()
-
-
-func width()->float:
-	return 100.0
-
-
-func _on_TileEnterArea_body_entered(_body):
-	emit_signal("tile_entered")
+# Remove the tile when it is offscreen
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
