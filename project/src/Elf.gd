440b6407fbb09bd63c818d97a2e6730a96e86284
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-12-21 21:34:44 -0500

    Handle mouse input directly through input map
    This replaces the _event approach. It is an attempt to fix a reported
    bug about some Apple devices needing one to both click and press
    spacebar at the same time.

--- project/src/Elf.gd
+++ project/src/Elf.gd
@@ -9,20 +9,8 @@ export var jump_power := 500
 export var short_jump_velocity_scale := 0.4
 
 var _velocity := Vector2.ZERO
-var _was_pressed_last_frame = false
 
 onready var _jump_sound := $JumpSound
-
-
-func _input(event):
-	if event is InputEventMouseButton and event.is_pressed():
-		if not _was_pressed_last_frame:
-			_was_pressed_last_frame = true
-			Input.action_press("jump")
-	elif event is InputEventMouseButton and not event.is_pressed():
-		if _was_pressed_last_frame:
-			_was_pressed_last_frame = false
-			Input.action_release("jump")
 
 
 func _physics_process(delta:float):
